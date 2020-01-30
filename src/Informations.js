64476af2ef735481095c8e595dec67925dc07298
Danilo Lučić <lucic.danilo94@gmail.com>
2020-01-30 15:25:46 +0100

    chart data

--- src/Informations.js
+++ src/Informations.js
@@ -1,5 +1,6 @@
 import React from "react";
 import News from "./News";
+import { Line } from "react-chartjs-2";
 
 class Informations extends React.Component {
   render() {
@@ -9,7 +10,6 @@ class Informations extends React.Component {
     console.log(chosenCryptocurrency);
     console.log(chosenCurrency);
     // API Requests
-
     /*
     var url =
       "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=EUR";
@@ -24,12 +24,39 @@ class Informations extends React.Component {
     var response = httpGet(url);
     console.log(response);
     */
-    var response = "sadasdasd";
+
+    // Chart Data
+    var chart = {
+      data: {
+        labels: ["1", "2", "3", "4", "5"],
+        datasets: [
+          {
+            label: "Video Mades",
+            backgroundColor: "rgba(255, 0 , 255, 0.75)",
+            data: [4, 5, 1, 10, 32, 2, 12]
+          },
+          {
+            label: "Subscriptions",
+            backgroundColor: "rgba(0, 255, 0, 0.75)",
+            data: [14, 15, 21, 0, 12, 4, 20]
+          }
+        ]
+      }
+    };
+    console.log(chart);
 
     return (
       <div>
         <h1>Informations</h1>
-        <div></div>
+        <div style={{ position: "relative", width: 600, height: 550 }}>
+          <h3>Chart Samples</h3>
+          <Line
+            options={{
+              responsive: true
+            }}
+            data={chart.data}
+          />
+        </div>
         <div className="news">
           <News />
         </div>
